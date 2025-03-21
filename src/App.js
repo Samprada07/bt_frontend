40696c4d0af53794e7e66918d69184312b2c018f
sumpu#07 <samprada2058@gmail.com>
2025-03-21 21:27:48 +0545

    Project Boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes,  useLocation } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Upload from './components/Upload';
+import AdminDashboard from './components/AdminDashboard';
+import PredictionHistory from "./components/PredictionHistory";
+import AdminAnalytics from './components/AdminAnalytics';
+import Navbar from './components/Navbar';
+//import ProtectedRoute from './components/ProtectedRoute';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ConditionalNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/predictionhistory" element={<PredictionHistory />} />
+        <Route path='/adminanalytics' element={<AdminAnalytics/>}/>
+
+        {/* Protected Routes for Normal Users */}
+        {/*<Route element={<ProtectedRoute allowedRole="user" />}>
+          <Route path="/upload" element={<Upload />} />
+        </Route>
+
+        {/* Protected Routes for Admin */}
+        {/*<Route element={<ProtectedRoute allowedRole="admin" />}>
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        </Route>*/}
+      </Routes>
+    </Router>
   );
 }
+
+function ConditionalNavbar() {
+  const location = useLocation(); // Get current route
+  const hideNavbarOn = [ "/", "/login", "/register"]; 
+
+  console.log("Current Path:", location.pathname); // Debugging
+
+  return hideNavbarOn.includes(location.pathname) ? null : <Navbar />;
+}
+
+
 
 export default App;
