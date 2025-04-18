cd9ce5252a3b7721c68f7681551ef9b709e83876
sumpu#07 <samprada2058@gmail.com>
2025-04-18 19:44:16 +0545

    Changes made in different files.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,62 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes,  useLocation } from 'react-router-dom';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import Upload from './components/Upload';
-import AdminDashboard from './components/AdminDashboard';
-import PredictionHistory from "./components/PredictionHistory";
-import AdminAnalytics from './components/AdminAnalytics';
-import Navbar from './components/Navbar';
-//import ProtectedRoute from './components/ProtectedRoute';
-
-
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	useLocation,
+} from "react-router-dom";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Register from "./containers/Register";
+import Upload from "./containers/Upload";
+import AdminDashboard from "./containers/AdminDashboard";
+import PredictionHistory from "./containers/PredictionHistory";
+import AdminAnalytics from "./containers/AdminAnalytics";
+import Navbar from "./containers/Navbar";
+import ProtectedRoute from "./containers/ProtectedRoute";
+import ForgotPassword from "./containers/ForgotPassword";
+import ResetPassword from "./containers/ResetPassword";
 
 function App() {
-  return (
-    <Router>
-      <ConditionalNavbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        <Route path="/predictionhistory" element={<PredictionHistory />} />
-        <Route path='/adminanalytics' element={<AdminAnalytics/>}/>
+	return (
+		<Router>
+			<ConditionalNavbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/forgotpassword" element={<ForgotPassword />} />
+				<Route path="/reset-password/:token" element={<ResetPassword />} />
 
-        {/* Protected Routes for Normal Users */}
-        {/*<Route element={<ProtectedRoute allowedRole="user" />}>
-          <Route path="/upload" element={<Upload />} />
-        </Route>
+				{/* Protected Routes for Normal Users  */}
+				<Route element={<ProtectedRoute allowedRole="Normal user" />}>
+					<Route path="/predictionhistory" element={<PredictionHistory />} />
+					<Route path="/upload" element={<Upload />} />
+				</Route>
 
-        {/* Protected Routes for Admin */}
-        {/*<Route element={<ProtectedRoute allowedRole="admin" />}>
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        </Route>*/}
-      </Routes>
-    </Router>
-  );
+				{/* Protected Routes for Admin */}
+				<Route element={<ProtectedRoute allowedRole="admin" />}>
+					<Route path="/admindashboard" element={<AdminDashboard />} />
+					<Route path="/adminanalytics" element={<AdminAnalytics />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
 }
 
 function ConditionalNavbar() {
-  const location = useLocation(); // Get current route
-  const hideNavbarOn = [ "/", "/login", "/register"]; 
+	const location = useLocation(); // Get current route
+	const hideNavbarOn = [
+		"/",
+		"/login",
+		"/register",
+		"/forgotpassword",
+		"/resetpassword/:token",
+	];
 
-  console.log("Current Path:", location.pathname); // Debugging
+	console.log("Current Path:", location.pathname); // Debugging
 
-  return hideNavbarOn.includes(location.pathname) ? null : <Navbar />;
+	return hideNavbarOn.includes(location.pathname) ? null : <Navbar />;
 }
-
-
 
 export default App;
